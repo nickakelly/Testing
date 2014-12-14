48c3f5ff6d4c0a579e032e61f85dd910bd51c01f
Nick Kelly <nicka.kelly@gmail.com>
2014-12-14 15:57:19 -0800

    asdf

--- creativekilter/index.php
+++ creativekilter/index.php
@@ -30,14 +30,7 @@
      <h1>Designed to Convert</h1><?php include('inc/contact.php'); ?>
      <div id="h1-border"></div>
      <div id="content">
-          <p>In an age where technology is rapidly expanding into every  aspect  of  our  lives  the  need  for  an  online  presence  has increased.
-          We live in a world where the media reigns supreme, a world where the individual’s attention span is perpetually shrinking due to constant exposure to flashy media.
-          The result of this never ending exposure to cleverly crafted “calls to action” has been as sense of standardization and form. It is no longer
-          enough to have good content and pretty design.</p>
-          <p>The website as we know it is continually undergoing transformation to suit the needs of today. Your website must be translated to fit the fine balanced between
-          content and design without compromising on the functionality. That’s where we come in. At Creative Kilter we design the website to suit your needs, allowing the
-          site itself to convert visitors into clients. Becoming part of the online world will open new avenues of networking and success. However, neglecting to join the race
-          will lead to relying on the dying pillars of marketing.</p>
+          <p>There is a hole in my bucket</p>
           <section id="cta">
                <a id="cta-quote" href="get-a-quote.php"></a><span id="or"></span><a id="cta-portfolio" href="portfolio.php"></a>
           </section>
